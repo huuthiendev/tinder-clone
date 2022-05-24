c21ffb3c8c6779e70db11b8d473fc03c76b64c89
huuthiendev <huuthiendev@gmail.com>
2022-05-25 05:32:50 +0700

    add cors module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import { AppRouting } from './routing';
 import * as dotenv from "dotenv";
 import { connectDb } from './database'; // initialize database
+import cors from 'cors';
 
 // Load env
 dotenv.config();
@@ -12,6 +13,7 @@ const app: Application = express();
 const router = express.Router();
 const APP_PORT = process.env.APP_PORT || 3333;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 // Routes
