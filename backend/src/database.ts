da602691a283bd755bc3040132c64d83b98c8aaa
huuthiendev <huuthiendev@gmail.com>
2022-05-25 00:25:47 +0700

    add routes

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 function connectDb() {
   // Build the connection string
-  const dbURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  const dbUser = process.env.DB_USER && process.env.DB_PASSWORD ? `${process.env.DB_USER}:${process.env.DB_PASSWORD}@` : null;
+  const dbURI = `mongodb://${dbUser ? dbUser : ''}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
   // Create the database connection
   mongoose
