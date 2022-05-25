0b28c7ccdd642c74d13c315a1daf52e922b04166
huuthiendev <huuthiendev@gmail.com>
2022-05-25 10:13:19 +0700

    update app js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-import { Grid, Container, BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { Grid, Container, BottomNavigation, BottomNavigationAction, LinearProgress } from '@mui/material';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import JoinInnerIcon from '@mui/icons-material/JoinInner';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 import DiscoverScreen from './pages/discover/discover';
+import constants from './utils/constants';
+import { getRandomUser } from './services/user.service';
 
 const BottomBar = () => {
   const [currentTab, setCurrentTab] = useState<any>('');
@@ -39,21 +41,48 @@ const BottomBar = () => {
 }
 
 const App = () => {
+  const [loginInfo, setLoginInfo] = useState<any>();
+
+  useEffect(() => {
+    // Check stored login info
+    var userStorage: any = localStorage.getItem(constants.STORAGE_KEY);
+
+    if (userStorage) {
+      var user = JSON.parse(userStorage);
+      setLoginInfo(user);
+    }
+    else {
+      // Get random user to login
+      getRandomUser().then((data) => {
+        if (data) {
+          localStorage.setItem(constants.STORAGE_KEY, JSON.stringify(data));
+          setLoginInfo(data);
+        }
+      });
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Container fixed style={{ height: '100vh', padding: 0 }}>
         <Grid container justifyContent={'center'} height={'100%'}>
           <Grid item md={6} xs={12} height={'100%'} padding={2} style={{ backgroundColor: 'white' }}>
-            <Grid height={'calc(100% - 72px)'} >
-              <Routes>
-                <Route path="/" element={<DiscoverScreen />} />
-              </Routes>
-            </Grid>
-            <BottomBar />
+            {
+              loginInfo
+                ? <React.Fragment>
+                  <Grid height={'calc(100% - 72px)'} >
+                    <Routes>
+                      <Route path="/" element={<DiscoverScreen loginInfo={loginInfo} />} />
+                    </Routes>
+                  </Grid>
+                  <BottomBar />
+                </React.Fragment>
+                : <LinearProgress />
+            }
           </Grid>
         </Grid>
       </Container>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 };
 
